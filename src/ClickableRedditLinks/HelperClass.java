cb832565cfe76e19eb1c0edf73ff8104306f8b01
TheFloatGoat <rickdejager99@gmail.com>
2017-11-04 11:36:46 +0100

    Preparing to merge back with master

--- src/ClickableRedditLinks/HelperClass.java
+++ src/ClickableRedditLinks/HelperClass.java
@@ -1,36 +1,37 @@
 package ClickableRedditLinks;
 
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class HelperClass {
 
-
-    String regex = "[^\\w\\[]\\/[ru]\\/\\w+?\\b"; //[non-word][/][char][/][r or u][up to word boundary]
+    String regex = "(?i)[^\\w\\[][ru]\\/[a-z0-9_^]*\\b"; //[non-word][/][r or u][/][word][up to word boundary]
 
     //ToBeReplacedWithStuff is a placeholder for the /r/subreddit or /u/user link, if a user uses 'ToBeReplacedWithStuff' in a chat message, stuff will break, but that's on them tbh.
     String link = " \"},{\"text\":\"[ToBeReplacedWithStuff]\",\"color\":\"gold\",\"bold\":true,\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://www.reddit.comToBeReplacedWithStuff\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Open https://www.reddit.comToBeReplacedWithStuff\",\"color\":\"blue\"}]}}},{\"text\":\" ";
 
 
     public String ReplaceAllLinks(String input) {
+
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(input);
+
         if (m.find()) {
+
             String subString = m.group(0).replaceAll("[\\s\"]",""); //Filter out all spaces and " so it doesn't get stuck in a recursive loop
             return ReplaceAllLinks(FilterMessage(input, subString));
-        }else{
+
+        }else {
             return input;
         }
     }
 
-
-
     public String FilterMessage(String message, String substring) {
+
         return message.replace(substring, link.replaceAll("ToBeReplacedWithStuff", substring));
     }
 
-    public boolean ContainsRedditLink(String input){
+    public boolean ContainsRedditLink(String input) {
 
         Pattern p = Pattern.compile(regex);
         Matcher m = p.matcher(input);
--- src/ClickableRedditLinks/ReplaceAllChat.java
+++ src/ClickableRedditLinks/ReplaceAllChat.java
@@ -2,7 +2,6 @@ package ClickableRedditLinks;
 
 
 import com.comphenix.protocol.PacketType;
-
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
 import com.comphenix.protocol.events.PacketContainer;
@@ -12,31 +11,36 @@ import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import org.bukkit.plugin.Plugin;
 
 
+/**
+ * This method replaces reddit links in all messages, including motd, tell. msg, etc
+ */
 public class ReplaceAllChat {
 
-    HelperClass hc = new HelperClass();
+    HelperClass hc = new HelperClass(); //Grab an object of the helperclass to use it's methods
 
+    /**
+     *This method set's up the onPacketSending listener
+     */
     public void init(Plugin pl) {
 
-        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(pl, PacketType.Play.Server.CHAT) {
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(pl, PacketType.Play.Server.CHAT) { //Add the packet listener
             @Override
             public void onPacketSending(PacketEvent event) {
                 PacketContainer packet = event.getPacket();
                 StructureModifier<WrappedChatComponent> chatComponents = packet.getChatComponents();
                 WrappedChatComponent msg = chatComponents.read(0);
-                String message = msg.getJson();
+                String message = msg.getJson(); //Get the message as a Json string
 
-                message = hc.ReplaceAllLinks(message);
+                message = hc.ReplaceAllLinks(message);  //Filter all links
                 try {
-                    msg.setJson(message);
+                    msg.setJson(message);               //Converting back to a WrappedChatComponent
                     chatComponents.write(0, msg);
                 } catch (Exception e) {
                     System.out.println("[ClickableRedditLinks] An error occurred while trying to convert back to json chat.");
                     return;
                 }
             }
-
-            });
+        });
 
     }
 }
